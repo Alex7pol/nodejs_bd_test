996eb3a3b8b7d835b62703e46f960fbe7c087ad6
Alex Polischuk <palex@blackduck.com>
2025-06-24 20:39:52 +0300

    add bug

--- main.js
+++ main.js
@@ -10,4 +10,13 @@ const phin = require('phin');
     }
 
     console.log(1/1)
+    try {
+        const response = await phin('https://google.com');
+        console.log('Status Code:', response.statusCode);
+        console.log('Response Body:', response.body.toString());
+    } catch (error) {
+        console.error('Request failed:', error);
+    }
+
+    console.log(1/0)
 })();
